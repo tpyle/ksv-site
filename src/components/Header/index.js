336238f28f39084837a80d7ba7f577ca63a83770
tpyle <thomasp162@gmail.com>
2020-10-13 00:33:14 +0000

    Adds a clear clipboard button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 
 export default ({ isLoggedIn })=>{
     const variant = isLoggedIn ? "success" : "warning";
     return (
         <Navbar sticky="top" bg={variant} variant="dark">
             <Navbar.Brand className="mx-auto" href="#">Password Manager</Navbar.Brand>
+            <CopyToClipboard text={" "}>
+                <Button variant="outline-light">
+                    Clear Clipboard
+                </Button>
+            </CopyToClipboard>
         </Navbar>
     );
 }
